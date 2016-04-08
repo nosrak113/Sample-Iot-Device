152aa4d6ecd29e9ed440830d538811e6535291fe
Karson Miller <karson@nack.in>
2016-04-08 12:44:46 -0700

    Quick and dirty Iot thermostat

--- Thermastat/ViewController.swift
+++ Thermastat/ViewController.swift
@@ -7,13 +7,53 @@
 //
 
 import UIKit
+import Alamofire
+
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var DeviceName : UITextField!
+    @IBOutlet var CapSlider : UISlider!
+    @IBOutlet var BatSlider : UISlider!
+    @IBOutlet var StateSegement : UISegmentedControl!
+    @IBOutlet var SendButton : UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        SendButton.addTarget(self, action: #selector(ViewController.sendPressed(_:)), forControlEvents: .TouchUpInside)
+    }
+    
+    
+    func sendPressed(sender: UIButton!) {
+        let url = "https://ingestion-r8xxs5mrp6s7.us3.sfdcnow.com/streams/device_data_home001/device_events_hom001/event"
+        let URL = NSURL(string: url)!
+        
+        
+
+        let header = ["Authorization": "Bearer 9njon90NAkISmq73jFwwlqS24u3ykfJAOVEIMfaNYW8eJEIDUvLYam4fEX6mc5RqNfRWTCVPRNugggzZxtle9n",
+                      "Content-Type":"application/json"]
+        
+        let data = ["D_id":self.DeviceName.text!,
+                    "Status": self.StateSegement.selectedSegmentIndex.description,
+                    "Operational_data": [
+                        "Cap_Voltage" : "\(self.CapSlider.value)",
+                        "Bat_Voltage" : "\(self.BatSlider.value)"
+                        ]
+                    
+                    ]
+        
+        
+        Alamofire.request(.POST, URL, parameters: (data as! [String : AnyObject]), encoding: ParameterEncoding.JSON, headers: header)
+        
+        
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
